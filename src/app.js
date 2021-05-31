0bc4554cd1220a1749545120b44d8fc3ed1bf04e
pcodexive <64474006+pcodexive@users.noreply.github.com>
2021-05-31 22:03:08 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 const documents = {};
-app.use(cors());
+app.use(cors({origin: '*'}));
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
