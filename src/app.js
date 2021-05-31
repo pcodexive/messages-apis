869a1061d672da29b3a40a4e4c74491d2e5a9acb
pcodexive <64474006+pcodexive@users.noreply.github.com>
2021-05-31 22:05:16 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,11 +5,24 @@ const io = require('socket.io')(http);
 
 const documents = {};
 app.use(cors({origin: '*'}));
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 io.on('connection', socket => {
     let previousId;
