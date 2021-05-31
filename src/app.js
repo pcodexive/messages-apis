cb2389977abbe4dabf9e54e543ada6a1a0ccfbab
pcodexive <64474006+pcodexive@users.noreply.github.com>
2021-05-31 21:27:34 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const app = require('express')();
+var cors = require('cors')
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 const documents = {};
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 io.on('connection', socket => {
     let previousId;
