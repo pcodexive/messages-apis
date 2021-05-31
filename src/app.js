99ab76dca76d7bb61e822571198665461fdafc2a
pcodexive <64474006+pcodexive@users.noreply.github.com>
2021-05-31 22:30:39 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,60 +1,77 @@
-const app = require('express')();
-var cors = require('cors')
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const app = require("express")();
+var cors = require("cors");
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true,
+        };
+        res.writeHead(200, headers);
+        res.end();
+    },
+});
 
 const documents = {};
-app.use(cors({origin: '*'}));
+app.use(cors({ origin: "*" }));
 app.use(function (req, res, next) {
-
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader("Access-Control-Allow-Credentials", true);
 
     // Pass to next layer of middleware
     next();
 });
-io.origins(["http://localhost:4444"]); // for local development
-io.origins(["https://message-angular.herokuapp.com"]);
-io.on('connection', socket => {
-    let previousId;
-    const safeJoin = currentId => {
-        socket.leave(previousId);
-        socket.join(currentId, () => console.log(`Socket ${socket.id} joined room ${currentId}`));
-        previousId = currentId;
-    }
 
-    socket.on('getDoc', docId => {
+io.on("connection", (socket) => {
+    let previousId;
+    const safeJoin = (currentId) => {
+        socket.leave(previousId);
+        socket.join(currentId, () =>
+            console.log(`Socket ${socket.id} joined room ${currentId}`)
+        );
+        previousId = currentId;
+    };
+
+    socket.on("getDoc", (docId) => {
         safeJoin(docId);
-        socket.emit('document', documents[docId]);
+        socket.emit("document", documents[docId]);
     });
 
-    socket.on('addDoc', doc => {
+    socket.on("addDoc", (doc) => {
         documents[doc.id] = doc;
         safeJoin(doc.id);
-        io.emit('documents', Object.keys(documents));
-        socket.emit('document', doc);
+        io.emit("documents", Object.keys(documents));
+        socket.emit("document", doc);
     });
 
-    socket.on('editDoc', doc => {
+    socket.on("editDoc", (doc) => {
         documents[doc.id] = doc;
-        socket.to(doc.id).emit('document', doc);
+        socket.to(doc.id).emit("document", doc);
     });
 
-    io.emit('documents', Object.keys(documents));
+    io.emit("documents", Object.keys(documents));
 
     console.log(`Socket ${socket.id} has connected`);
 });
 
-http.listen(4444, () => {
-    console.log('Listening on port 4444');
+server.listen(4444, () => {
+    console.log("Listening on port 4444");
 });
